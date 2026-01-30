7e455a193e882cd30f961eca2612a4655441deef
Keysha Kirana <reydreg4s@gmail.com>
2026-01-30 19:27:40 +0700

    Update Create Peminjaman Admin dan Petugas

--- app/Helpers/activity_helper.php
+++ app/Helpers/activity_helper.php
@@ -2,16 +2,16 @@
 
 use App\Models\ActivityLogModel;
 
-function log_activity($aktivitas, $tabel = null, $id_data = null)
+function log_activity($aktivitas, $tabel = null, $id_data = null, $idUser = null)
 {
     $logModel = new ActivityLogModel();
 
     $logModel->insert([
-        'id_user'  => session('id_user'),
-        'role'     => session('role'),
-        'aktivitas'=> $aktivitas,
-        'tabel'    => $tabel,
-        'id_data'  => $id_data,
-        'created_at' => date('Y-m-d H:i:s')
+        'id_user'   => $idUser ?? session('id_user'),
+        'role'      => session('role'),
+        'aktivitas' => $aktivitas,
+        'tabel'     => $tabel,
+        'id_data'   => $id_data,
+        'created_at'=> date('Y-m-d H:i:s')
     ]);
 }
